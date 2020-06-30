dcdc223780423a187b9d05d8805ccb87bf4ed171
Matheus Michalski Puel <matheuspuel@gmail.com>
2020-06-29 23:07:03 -0300

    test ci/cd tests

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+# class TestTest(SimpleTestCase):
+#     def test_first_test(self):
+#         assert 2 == 2, 'My first test'
+#         assert 1 == 1, 'My sec test'
+#
+#     def test_second_test(self):
+#         assert 1 == 1
+#
+#
+# class TestSecType(TestCase):
+#     def setUp(self):
+#         self.a = 1
+#
+#     def test_one(self):
+#         self.assertEquals(self.a, 1, 'Should be right')
+
+
+class TestViews(TestCase):
+
+    def test_response(self):
+        response = self.client.get(reverse('login'))
+        self.assertEquals(response.status_code, 200, 'Login page should be accessible')
+
+    def test_404(self):
+        response = self.client.get('/lsdfjkdskfjsdklfj')
+        self.assertEquals(response.status_code, 404, 'Incorrect URL should return 404 error')
+
+    def test_ci_cd(self):
+        self.assertTrue(False, 'Testing Heroku CI/CD')
+
+
+
